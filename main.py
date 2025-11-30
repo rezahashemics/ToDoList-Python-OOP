2c6e6d8d4d89088cde174525db062fce8f773233
rezahashemics <rezahashemipersonalemail@gmail.com>
2025-11-30 22:48:14 +0330

    rewriting the main file base on the fast api

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from src.cli.console import CLI
+from fastapi import FastAPI
+from src.api.v1.routers import projects # Import the project router
 
-if __name__ == "__main__":
-    cli = CLI()
-    cli.run()
+
+app = FastAPI(
+    title="ToDoList API",
+    description="A RESTful API for managing ToDo Projects and Tasks.",
+    version="1.0.0",
+)
+
+# 1. Include Routers (Controllers)
+app.include_router(projects.router, prefix="/v1")
+
+
+@app.get("/", tags=["Root"])
+async def root():
+    return {"message": "Welcome to the ToDoList Web API! Go to /docs for documentation."}
+
+
